b749a03e8331c37e1ef316d1fea00f5187fce0e1
mro73 <klaudiamoz0@gmail.com>
2024-05-29 10:50:23 +0200

    Fix problems

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,26 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from bs4 import BeautifulSoup
+import requests
 
 @app.route('/')
 def index():
     return render_template('index.html.jinja')
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST', 'GET'])
 def extract():
+    if request.method == "POST":
+        product_id = request.form.get("product_id")
+        url = f"https://www.ceneo.pl/{product_id}"
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            page = BeautifulSoup(response.text, "html.parser")
+            opinions_count = page.select_one("a.product-review__link > span")
+            if opinions_count:  
+            # ekstrakcja
+                return redirect(url_for('product', product_id=product_id))
+            return render_template('extract.html.jinja', error="Produkt o podanym kodzie nie posiada opinii.")
+        return render_template('extract.html.jinja', error="Produkt o podanym kodzie nie istnieje")
     return render_template('extract.html.jinja')
 
 @app.route('/products')
@@ -17,6 +31,6 @@ def products():
 def about():
     return render_template('about.html.jinja')
 
-@app.route('/product')
+@app.route('/product/<product_id>')
 def product(product_id):
     return render_template('product.html.jinja', product_id=product_id)
